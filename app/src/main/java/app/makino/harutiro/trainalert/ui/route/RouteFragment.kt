68bef1dcde85eb58185f35a49fd93cdf83a1ef70
harutiro <harutiro2027@outlook.jp>
2021-12-29 22:19:13 +0900

    add: ベルの部分をコード側から変更できるようにする方法を見つけた

--- app/src/main/java/app/makino/harutiro/trainalert/ui/route/RouteFragment.kt
+++ app/src/main/java/app/makino/harutiro/trainalert/ui/route/RouteFragment.kt
@@ -2,6 +2,8 @@ package app.makino.harutiro.trainalert.ui.route
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Switch
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import app.makino.harutiro.trainalert.R
 
@@ -9,5 +11,7 @@ class RouteFragment : Fragment(R.layout.fragment_route) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<Switch>(R.id.switch2).thumbDrawable = ResourcesCompat.getDrawable(resources, R.drawable.true_bell, null)
     }
 }
