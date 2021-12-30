138141a4f034b6c6df38149efce7c3bff1dbffba
harutiro <harutiro2027@outlook.jp>
2021-12-31 00:40:14 +0900

    fix:自作のトグルボタン、スイッチ　完成

--- app/src/main/java/app/makino/harutiro/trainalert/ui/map/MapFragment.kt
+++ app/src/main/java/app/makino/harutiro/trainalert/ui/map/MapFragment.kt
@@ -1,13 +1,17 @@
 package app.makino.harutiro.trainalert.ui.map
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import app.makino.harutiro.trainalert.EditActivity
 import app.makino.harutiro.trainalert.R
 
 class MapFragment : Fragment(R.layout.fragment_map) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        startActivity(Intent(context,EditActivity::class.java))
     }
 }
