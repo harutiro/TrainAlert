3447caa35c711d00754fa05d42ce6ba16bd2792a
harut <harutiro2027@outlook.jp>
2022-01-02 13:23:13 +0900

    err: fragmentを呼び出すことができない。　orz

--- app/src/main/java/app/makino/harutiro/trainalert/ui/map/MapFragment.kt
+++ app/src/main/java/app/makino/harutiro/trainalert/ui/map/MapFragment.kt
@@ -2,16 +2,40 @@ package app.makino.harutiro.trainalert.ui.map
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import app.makino.harutiro.trainalert.EditActivity
 import app.makino.harutiro.trainalert.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MapFragment : Fragment(R.layout.fragment_map) {
+class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.mapMapView) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
         startActivity(Intent(context,EditActivity::class.java))
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
     }
 }
