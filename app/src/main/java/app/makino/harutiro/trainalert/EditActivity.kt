72e511633713f0291e96fd4f04f1ef711142698e
harutiro <harutiro2027@outlook.jp>
2022-01-03 12:08:29 +0900

    add:placesにより場所の取得の可能
    API側でお支払いの設定をしていなかったから、
    
    おそらく、使うAPI間違えた。
    
    プライスIDを取得できれば動く？？

--- app/src/main/java/app/makino/harutiro/trainalert/EditActivity.kt
+++ app/src/main/java/app/makino/harutiro/trainalert/EditActivity.kt
@@ -1,19 +1,64 @@
 package app.makino.harutiro.trainalert
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.android.libraries.places.api.net.FetchPlaceRequest
+import java.util.*
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.android.gms.tasks.OnSuccessListener
+import com.google.android.libraries.places.api.net.FetchPlaceResponse
+import java.lang.Exception
 
 
+class EditActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    val tag = "debag"
 
-class EditActivity : AppCompatActivity() , OnMapReadyCallback{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
 
+
+
+        Places.initialize(application, "AIzaSyCbnAj8bhSfWi4vuDTZa--6OnnFk7VUm7g")
+
+        val placesClient = Places.createClient(this)
+
+        // Define a Place ID.
+        val placeId = "EicxMyBNYXJrZXQgU3QsIFdpbG1pbmd0b24sIE5DIDI4NDAxLCBVU0EiGhIYChQKEgnRTo6ixx-qiRHo_bbmkCm7ZRAN"
+
+// Specify the fields to return.
+        val placeFields = listOf(Place.Field.ID, Place.Field.NAME)
+
+// Construct a request object, passing the place ID and fields array.
+        val request = FetchPlaceRequest.newInstance(placeId, placeFields)
+
+        placesClient.fetchPlace(request)
+            .addOnSuccessListener { response: FetchPlaceResponse ->
+                val place = response.place
+                Log.d("debug", "Place found: ${place.name}")
+            }.addOnFailureListener { exception: Exception ->
+                if (exception is ApiException) {
+                    Log.d("debug", "Place not found: ${exception.message}")
+                    val statusCode = exception.statusCode
+                }
+            }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
 
     }
