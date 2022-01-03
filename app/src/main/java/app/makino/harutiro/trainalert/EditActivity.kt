0f7920285eb8870e29fb6abeeb29f02a3b6cc334
harutiro <harutiro2027@outlook.jp>
2022-01-03 11:06:42 +0900

    add:places mapの追加

--- app/src/main/java/app/makino/harutiro/trainalert/EditActivity.kt
+++ app/src/main/java/app/makino/harutiro/trainalert/EditActivity.kt
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.common.api.GoogleApiClient
+
+
+
 
 class EditActivity : AppCompatActivity() , OnMapReadyCallback{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit)
+
+
+
     }
 
     override fun onMapReady(p0: GoogleMap) {
