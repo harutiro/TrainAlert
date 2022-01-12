0a8e3404cfa6ee241037d97c77a337daf8ca2281
harutiro <harutiro2027@outlook.jp>
2022-01-12 22:59:14 +0900

    add: Googleによるルート検索の部分完成
    しかし、乗換案内はやってくれない。
    日本では対応してない。

--- app/src/main/java/app/makino/harutiro/trainalert/EditActivity.kt
+++ app/src/main/java/app/makino/harutiro/trainalert/EditActivity.kt
@@ -4,9 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.common.api.GoogleApiClient
 import com.google.android.gms.common.api.Status
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
@@ -19,6 +19,23 @@ import com.google.android.gms.tasks.OnFailureListener
 import com.google.android.gms.tasks.OnSuccessListener
 import com.google.android.libraries.places.api.net.FetchPlaceResponse
 import java.lang.Exception
+import com.google.gson.GsonBuilder
+
+import com.google.gson.Gson
+import com.google.maps.DirectionsApi
+
+import com.google.maps.GeocodingApi
+
+import com.google.maps.model.GeocodingResult
+
+import com.google.maps.GeoApiContext
+import com.google.maps.GeocodingApi.geocode
+import com.google.maps.model.TravelMode
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 
 
 class EditActivity : AppCompatActivity(), OnMapReadyCallback {
@@ -62,13 +79,13 @@ class EditActivity : AppCompatActivity(), OnMapReadyCallback {
                     as AutocompleteSupportFragment
 
         // Specify the types of place data to return.
-        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME ,Place.Field.ADDRESS ,Place.Field.LAT_LNG))
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME ,Place.Field.ADDRESS ,Place.Field.LAT_LNG ,Place.Field.TYPES))
 
         // Set up a PlaceSelectionListener to handle the response.
         autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
             override fun onPlaceSelected(place: Place) {
                 // TODO: Get info about the selected place.
-                Log.d("debug", "Place: ${place.name}, ${place.id} ,${place.address},${place.latLng}")
+                Log.d("debug", "Place: ${place.name}, ${place.id},${place.types} ,${place.address},${place.latLng}")
             }
 
             override fun onError(status: Status) {
@@ -76,6 +93,43 @@ class EditActivity : AppCompatActivity(), OnMapReadyCallback {
                 Log.d("debug", "An error occurred: $status")
             }
         })
+
+
+
+        //情報取得を別のスレッドでおこなうようにする
+        runBlocking(Dispatchers.IO){
+            runCatching {
+                val client: OkHttpClient = OkHttpClient().newBuilder()
+                    .build()
+                val requests: Request = Request.Builder()
+                    .url("https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=AIzaSyCWlZTj9siY9TFizSf06yrt55FaDmIZLjc")
+                    .method("GET", null)
+                    .build()
+                val response: Response = client.newCall(requests).execute()
+                Log.d("debug",response.toString())
+            }
+        }.onSuccess{
+
+        }.onFailure {
+            //失敗した時のところ。
+            Toast.makeText(this,"失敗",Toast.LENGTH_LONG).show()
+        }
+
+
+        val context = GeoApiContext.Builder()
+            .apiKey("AIzaSyCWlZTj9siY9TFizSf06yrt55FaDmIZLjc")
+            .build()
+        val results = DirectionsApi.newRequest(context)
+            .origin("愛知御津駅")
+            .destination("豊橋駅")
+            .mode(TravelMode.DRIVING)
+            .await()
+
+        val gson = GsonBuilder().setPrettyPrinting().create()
+        Log.d("debug2", "====================================================")
+        Log.d("debug2", gson.toJson(results))
+
+//        context.shutdown()
 
     }
 
