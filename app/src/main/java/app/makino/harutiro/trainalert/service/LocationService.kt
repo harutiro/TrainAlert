857b1be11042c23499a1a05a0f139f4823b9db12
harut <harutiro2027@outlook.jp>
2022-01-15 19:07:19 +0900

    add:位置情報を取得できるようにしたForeGroundで

---
+++ app/src/main/java/app/makino/harutiro/trainalert/service/LocationService.kt
@@ -0,0 +1,97 @@
+package app.makino.harutiro.trainalert.service
+
+import android.annotation.SuppressLint
+import android.app.PendingIntent
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import app.makino.harutiro.trainalert.MainActivity
+import app.makino.harutiro.trainalert.R
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.LocationRequest
+
+class LocationService : Service() {
+    companion object {
+        const val CHANNEL_ID = "777"
+    }
+
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+
+    override fun onCreate() {
+        super.onCreate()
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        var updatedCount = 0
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                p0 ?: return
+                for (location in p0.locations){
+                    updatedCount++
+                    Log.d("debug", "[${updatedCount}] ${location.latitude} , ${location.longitude}")
+                }
+            }
+        }
+
+        val openIntent = Intent(this, MainActivity::class.java).let {
+            PendingIntent.getActivity(this, 0, it, 0)
+        }
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("位置情報テスト")
+            .setContentText("位置情報を取得しています...")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(openIntent)
+            .build()
+
+        startForeground(9999, notification)
+
+        startLocationUpdates()
+
+        return START_STICKY
+    }
+
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun stopService(name: Intent?): Boolean {
+        return super.stopService(name)
+        stopLocationUpdates()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        stopLocationUpdates()
+        stopSelf()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        val locationRequest = createLocationRequest() ?: return
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            null)
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    private fun createLocationRequest(): LocationRequest? {
+        return LocationRequest.create()?.apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+    }
+}
