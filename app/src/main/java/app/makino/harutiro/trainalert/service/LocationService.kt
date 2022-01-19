7522d6fd17cbbb1f755dd66e35c5e83f92fc52a7
harutiro <harutiro2027@outlook.jp>
2022-01-20 07:02:21 +0900

    add:LocationServise DBからデータを持ってきて距離計算まで完成！！
    あとは通知のみ

--- app/src/main/java/app/makino/harutiro/trainalert/service/LocationService.kt
+++ app/src/main/java/app/makino/harutiro/trainalert/service/LocationService.kt
@@ -9,16 +9,26 @@ import android.util.Log
 import androidx.core.app.NotificationCompat
 import app.makino.harutiro.trainalert.MainActivity
 import app.makino.harutiro.trainalert.R
+import app.makino.harutiro.trainalert.dateBase.RouteDateClass
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.LocationRequest
+import io.realm.Realm
+import kotlin.math.acos
+import kotlin.math.cos
+import kotlin.math.sin
 
 class LocationService : Service() {
     companion object {
         const val CHANNEL_ID = "777"
     }
+
+    private val realm by lazy {
+        Realm.getDefaultInstance()
+    }
+
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationCallback: LocationCallback
@@ -29,6 +39,8 @@ class LocationService : Service() {
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val realmResalt = realm.where(RouteDateClass::class.java).findAll()
+
         var updatedCount = 0
         locationCallback = object : LocationCallback() {
             override fun onLocationResult(p0: LocationResult) {
@@ -36,6 +48,13 @@ class LocationService : Service() {
                 for (location in p0.locations){
                     updatedCount++
                     Log.d("debag2", "[${updatedCount}] ${location.latitude} , ${location.longitude}")
+
+                    for(i in realmResalt){
+                        for(j in i.routeList!!){
+                            Log.d("debag3","${i.routeName},${j.placeLovalLanguageName}")
+                            Log.d("debag3","${getDistance(location.latitude,location.longitude,j.placeLat,j.placeLon,'k')}")
+                        }
+                    }
                 }
             }
         }
@@ -93,5 +112,40 @@ class LocationService : Service() {
             fastestInterval = 5000
             priority = LocationRequest.PRIORITY_HIGH_ACCURACY
         }
+    }
+
+    /*
+     * 2点間の距離を取得
+     * 第五引数に設定するキー（unit）で単位別で取得できる
+     */
+    private fun getDistance(
+        lat1: Double,
+        lon1: Double,
+        lat2: Double,
+        lon2: Double,
+        unit: Char
+    ): Double {
+        val theta = lon1 - lon2
+        var dist =
+            sin(deg2rad(lat1)) * sin(deg2rad(lat2)) + cos(deg2rad(lat1)) * cos(
+                deg2rad(lat2)
+            ) * cos(deg2rad(theta))
+        dist = acos(dist)
+        dist = rad2deg(dist)
+        val miles = dist * 60 * 1.1515
+        return when (unit) {
+            'K' -> miles * 1.609344
+            'N' -> miles * 0.8684
+            'M' -> miles
+            else -> miles
+        }
+    }
+
+    private fun rad2deg(radian: Double): Double {
+        return radian * (180f / Math.PI)
+    }
+
+    fun deg2rad(degrees: Double): Double {
+        return degrees * (Math.PI / 180f)
     }
 }
