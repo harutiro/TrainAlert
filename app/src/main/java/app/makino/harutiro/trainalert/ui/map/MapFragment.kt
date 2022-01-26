4f8351f80137c8ef119018bf39c6742253905573
harutiro <harutiro2027@outlook.jp>
2022-01-26 19:18:21 +0900

    bag: mapFragmentが操作できない また、落ちる問題解決

--- app/src/main/java/app/makino/harutiro/trainalert/ui/map/MapFragment.kt
+++ app/src/main/java/app/makino/harutiro/trainalert/ui/map/MapFragment.kt
@@ -10,6 +10,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
@@ -26,30 +27,38 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.tasks.Task
 
 
-class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback {
+class MapFragment : Fragment() {
 
-    private lateinit var mMap: GoogleMap
 
-    private lateinit var locationCallback: LocationCallback
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        val sydney = LatLng(-34.0, 151.0)
+        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-//        ＝＝＝＝＝＝＝マップ関係の試行錯誤部分
-//        val mapFragment = getSupportFragmentManager()
-//            .findFragmentById(R.id.mapFragment) as SupportMapFragment
-//        mapFragment.getMapAsync(this)
-
-
-        
-
-
-
-
-
-
+        val mapFragment = childFragmentManager.findFragmentById(R.id.mapFragment) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
     }
+
 
     fun parmission(){
         val locationPermissionRequest = registerForActivityResult(
@@ -72,52 +81,5 @@ class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback {
         locationPermissionRequest.launch(arrayOf(
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.ACCESS_COARSE_LOCATION))
-    }
-
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_map, container, false)
-    }
-
-
-    override fun onDestroyView(){
-        super.onDestroyView()
-
-//        TODO:二重でマップフラグメントが呼ばれるのが原因っぽい　対策まだ
-        val f = fragmentManager?.findFragmentById(R.id.mapFragment) as MapFragment?
-        if (f != null) requireFragmentManager().beginTransaction().remove(f).commit()
-
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        //シドニーの表示
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
-//        ツールバーの表示
-        mMap.uiSettings.isMapToolbarEnabled = true
-
-//        現在地点の出力
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            //                TODO:ほんとはここでパーミッションの表示をするようにする
-
-        }else{
-            mMap.isMyLocationEnabled = true
-
-        }
     }
 }
