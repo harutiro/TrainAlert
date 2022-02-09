82019b0c52446246f028d8aef1458fea9cc8dd74
harutiro <harutiro2027@outlook.jp>
2022-02-10 00:10:36 +0900

    fix:スリープになるとバイブしなくなる現象を解消

--- app/src/main/java/app/makino/harutiro/trainalert/service/LocationService.kt
+++ app/src/main/java/app/makino/harutiro/trainalert/service/LocationService.kt
@@ -2,43 +2,28 @@ package app.makino.harutiro.trainalert.service
 
 import android.annotation.SuppressLint
 import android.app.*
-import android.content.Context
-import android.content.Intent
-import android.graphics.BitmapFactory
-import android.graphics.Color
-import android.media.AudioManager
-import android.net.Uri
-import android.util.Log
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import app.makino.harutiro.trainalert.MainActivity
-import app.makino.harutiro.trainalert.R
-import app.makino.harutiro.trainalert.dateBase.RouteDateClass
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationResult
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.location.LocationRequest
-import io.realm.Realm
-import kotlin.math.acos
-import kotlin.math.cos
-import kotlin.math.sin
-import android.app.NotificationManager
-import android.media.AudioAttributes
-import app.makino.harutiro.trainalert.EditActivity
-import okhttp3.internal.notify
-import android.content.BroadcastReceiver
-import android.os.*
-import android.app.PendingIntent
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothHeadset
 import android.bluetooth.BluetoothProfile
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.content.IntentFilter
-import android.widget.Toast
-import app.makino.harutiro.trainalert.dateBase.RouteListDateClass
-import kotlinx.coroutines.channels.BroadcastChannel
+import android.media.AudioManager
+import android.os.*
+import android.util.Log
+import android.view.WindowManager
+import androidx.core.app.NotificationCompat
+import app.makino.harutiro.trainalert.MainActivity
+import app.makino.harutiro.trainalert.R
+import app.makino.harutiro.trainalert.dateBase.RouteDateClass
+import com.google.android.gms.location.*
+import io.realm.Realm
 import java.util.*
+import kotlin.math.acos
+import kotlin.math.cos
+import kotlin.math.sin
 
 
 class LocationService : Service() {
@@ -67,7 +52,7 @@ class LocationService : Service() {
     }
 
 
-    @SuppressLint("NewApi")
+    @SuppressLint("NewApi", "LaunchActivityFromNotification")
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
 //        =====================イヤホン装着状態の確認
@@ -82,72 +67,6 @@ class LocationService : Service() {
 //        ＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝通知部分
 //        マネージャーのインスタンス化
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        // カテゴリー名（通知設定画面に表示される情報）
-        val name = "通常アラーム"
-        // システムに登録するChannelのID
-        val id = "TrainDefaultAlertChannel"
-        // 通知の詳細情報（通知設定画面に表示される情報）
-        val notifyDescription = "アラームの詳しい設定を行います"
-
-        // Channelの取得と生成
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O &&notificationManager.getNotificationChannel(id) == null) {
-//            デフォルト音声についての設定
-            val attributes = AudioAttributes.Builder().apply {
-                setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-                setUsage(AudioAttributes.USAGE_NOTIFICATION_RINGTONE)
-            }.build()
-//            チャンネルの重要度の設定
-            val mChannel = NotificationChannel(id, name, NotificationManager.IMPORTANCE_HIGH)
-//            着信音の設定
-            mChannel.setSound(Uri.parse("android.resource://$packageName/${R.raw.alert}"), attributes)
-//            通知チャンネルの詳細表示
-            mChannel.description = notifyDescription
-//            バイブの許可
-            mChannel.enableVibration(true)
-//            ？？？
-            mChannel.canShowBadge();
-//            LEDの許可
-            mChannel.enableLights(true);
-//            ？？？
-            mChannel.lockscreenVisibility = Notification.VISIBILITY_PRIVATE;
-//            ？？？
-            mChannel.setShowBadge(true);
-//            チャンネルの追加
-            notificationManager.createNotificationChannel(mChannel)
-        }
-
-        //通知にタップで反応するレシーバーを作成
-        val test_intent = Intent(this,StopAlertRecever::class.java) //空のインテントを準備
-        val test_pendingIntent = PendingIntent.getBroadcast(
-            baseContext,
-            0,
-            test_intent,
-            PendingIntent.FLAG_MUTABLE
-        ) //インテントをペンディングインテントに組み込む
-
-        //通常のタップでMainに飛ぶ通知
-        val intent = Intent(this, MainActivity::class.java).apply {}
-        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
-
-//        通知の作成
-        val notification2 = NotificationCompat.Builder(this, id)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle("title")
-            .setContentText("message")
-            .setPriority(NotificationCompat.PRIORITY_HIGH) // ② 通知の重要度
-            .setCategory(NotificationCompat.CATEGORY_EVENT) // ③ 通知のカテゴリ
-            .setDefaults(NotificationCompat.DEFAULT_ALL)
-            .setFullScreenIntent(pendingIntent, true)
-            .setVibrate(longArrayOf(0,1000,0,1000))
-            .addAction(  // 4. 「応答」ボタンを追加
-                R.drawable.false_bell,
-                "止める",
-                test_pendingIntent
-            )
-            .build()
-        notification2.flags = Notification.FLAG_ONLY_ALERT_ONCE or Notification.FLAG_NO_CLEAR or Notification.FLAG_INSISTENT
-
 
         // カテゴリー名（通知設定画面に表示される情報）
         val nonEarName = "イヤホンがないときのアラーム"
@@ -188,25 +107,16 @@ class LocationService : Service() {
             PendingIntent.FLAG_MUTABLE
         ) //インテントをペンディングインテントに組み込む
 
-        //通常のタップでMainに飛ぶ通知
-        val intent1 = Intent(this, MainActivity::class.java).apply {}
-        val pendingIntent1: PendingIntent = PendingIntent.getActivity(this, 0, intent1, PendingIntent.FLAG_IMMUTABLE)
-
 //        通知の作成
         val notification3 = NotificationCompat.Builder(this, nonEarId)
             .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle("title")
-            .setContentText("message")
+            .setContentTitle("駅にもうじき到着します")
+            .setContentText("通知をタップして止めましょう！！")
             .setPriority(NotificationCompat.PRIORITY_HIGH) // ② 通知の重要度
             .setCategory(NotificationCompat.CATEGORY_ALARM) // ③ 通知のカテゴリ
             .setDefaults(NotificationCompat.DEFAULT_ALL)
-            .setFullScreenIntent(pendingIntent1, true)
+            .setContentIntent(test_pendingIntent1)
             .setVibrate(longArrayOf(0,1000,0,1000))
-            .addAction(  // 4. 「応答」ボタンを追加
-                R.drawable.false_bell,
-                "止める",
-                test_pendingIntent1
-            )
             .build()
         notification3.flags = Notification.FLAG_ONLY_ALERT_ONCE or Notification.FLAG_NO_CLEAR or Notification.FLAG_INSISTENT
 
@@ -243,7 +153,7 @@ class LocationService : Service() {
                         if (distance!! <= 0.600 && routeDateUUID.isEmpty() && routeListDateUUID.isEmpty()) {
 
                                 if((isBluetoothHeadsetConnected || isEarphoneConnected) ){
-                                    notificationManager.notify(99, notification2)
+//                                    notificationManager.notify(99, notification2)
                                 }else{
                                     notificationManager.notify(102, notification3)
                                 }
