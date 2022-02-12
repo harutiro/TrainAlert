caa9489020294ab1de2aa75f245ce5a97a085a3f
harutiro <harutiro2027@outlook.jp>
2022-02-12 13:58:10 +0900

    add: settingFragment 設定画面の完成

--- app/src/main/java/app/makino/harutiro/trainalert/ui/setting/SettingFragment.kt
+++ app/src/main/java/app/makino/harutiro/trainalert/ui/setting/SettingFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceFragmentCompat
 import app.makino.harutiro.trainalert.EditActivity
 import app.makino.harutiro.trainalert.MainActivity2
 import app.makino.harutiro.trainalert.R
@@ -15,6 +16,20 @@ class SettingFragment : Fragment(R.layout.fragment_setting) {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<Button>(R.id.gotoButton).setOnClickListener{
             startActivity(Intent(context, MainActivity2::class.java))
+        }
+
+        if (savedInstanceState == null) {
+            childFragmentManager
+                .beginTransaction()
+                .replace(R.id.settings, SettingsFragment())
+                .commit()
+        }
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    class SettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey)
         }
     }
 }
