4a9d38966702aa1e9fef49cf4746db36b77c095a
harutiro <harutiro2027@outlook.jp>
2022-03-11 02:06:31 +0900

    fix:サービスが二重で起動してしまう問題解決

--- app/src/main/java/app/makino/harutiro/trainalert/StopLocationServiceActivity.kt
+++ app/src/main/java/app/makino/harutiro/trainalert/StopLocationServiceActivity.kt
@@ -1,25 +1,17 @@
 package app.makino.harutiro.trainalert
 
 import android.content.Intent
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import app.makino.harutiro.trainalert.service.LocationService
 
-class OpenLocationServiceActivity : AppCompatActivity() {
+class StopLocationServiceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.open_location_service_activity)
+        setContentView(R.layout.activity_stop_location_service)
 
         val targetIntent = Intent(this, LocationService::class.java)
         this.stopService(targetIntent)
-
-        val intent = Intent(this, LocationService::class.java)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(intent)
-        }else{
-            startService(intent)
-        }
         finish()
     }
 }
